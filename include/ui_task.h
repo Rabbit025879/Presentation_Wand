96755322293ca61035ba5dbdaea26c0294d097d9
rabbit025879 <rabbit025879@gmail.com>
2026-01-14 21:40:58 +0800

    feat: Implement UI task management for Haptics and Button tasks

---
+++ include/ui_task.h
@@ -0,0 +1,18 @@
+#ifndef UI_TASK_CPP
+#define UI_TASK_CPP
+
+#include "Haptics.h"
+#include "Button.h"
+
+namespace HapticsTask {
+  static void haptics_task(void *arg);
+  void haptics_task_start(QueueHandle_t q, uint8_t pin);
+//   bool execute_haptics(ButtonEvent evt, Haptics& haptics);
+} // namespace HapticsTask
+
+namespace ButtonTask {
+  static void button_task(void *arg);
+  void button_task_start(QueueHandle_t q, uint8_t pin);
+} // namespace ButtonTask
+
+#endif // UI_TASK_CPP
