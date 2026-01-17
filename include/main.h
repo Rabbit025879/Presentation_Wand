45fd5a4cd12025cf3ebfd3928ab2b96f96584b38
rabbit025879 <rabbit025879@gmail.com>
2026-01-17 14:25:12 +0800

    feat: Refactor main setup to integrate MPU task handling and streamline event queue creation

--- include/main.h
+++ include/main.h
@@ -1,13 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include "MPU.h"
 #include "button_task.h"
 #include "haptics_task.h"
 #include "laser_task.h"
 #include "hid_task.h"
+#include "mpu_task.h"
 #include "ota_task.h"
-
-MPU mpu;
 
 #endif // MAIN_H
